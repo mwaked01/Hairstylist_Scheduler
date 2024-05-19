dcc5b16557fc7b7615e1c4c4ed13a2a5083a92d0
Mohamed Waked <mwaked01@gmail.com>
2024-05-19 00:20:58 -0400

    fix drpricated options error

--- server/app.js
+++ server/app.js
@@ -9,14 +9,11 @@ const app = express();
 app.use(bodyParser.json());
 
 // Database connection
-mongoose.connect('mongodb://localhost:27017/appointments', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-
-}).then(() => {
+mongoose.connect('mongodb://localhost:27017/appointments', {})
+.then(() => {
   console.log('MongoDB connected');
-}).catch((err) => {
-  console.error('Error connecting to MongoDB', err);
+}).catch((error) => {
+  console.error('Error connecting to MongoDB',  error.message);
   process.exit(1); // Exit the process if connection fails
 });
 
