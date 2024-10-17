33a763938e82d5c8701aeb977d83754209c7618c
Mohamed Waked <134574251+mwaked01@users.noreply.github.com>
2024-10-17 03:14:34 -0400

    style photo gallery

--- client/src/components/HomeComponents/PhotoGallery.jsx
+++ client/src/components/HomeComponents/PhotoGallery.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 // import InstagramEmbed from 'react-instagram-embed';
 
 const PhotoGallery = (props) => {
-const [pic,setPic] = useState(1)
+  const [pic, setPic] = useState(1)
   return (
-    <div>
-    <button onClick={()=>{pic >1?setPic(pic-1):setPic(3)}}>Prev</button>
-    <button onClick={()=>{pic <3?setPic(pic+1):setPic(1)}}>Next</button>
-    <img src={`/insta${pic}.PNG`} alt={`Inspo Pic ${pic}`}/>
+    <div id="PhotoGallery">
+      <button onClick={() => { pic > 1 ? setPic(pic - 1) : setPic(3) }}><ArrowCircleLeftIcon/></button>
+      <img src={`/insta${pic}.PNG`} alt={`Inspo Pic ${pic}`} />
+      <button onClick={() => { pic < 3 ? setPic(pic + 1) : setPic(1) }}><ArrowCircleRightIcon/></button>
     </div>
   );
 };
